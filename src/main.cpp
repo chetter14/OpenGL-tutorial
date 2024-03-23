f39895a163eb4ec757335df357c4cf6baf2a8e38
artemleshchukov02 <artemleshchukov02@gmail.com>
2024-03-23 22:45:45 +0400

    break down pieces of code into logical functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,67 @@
 #include <iostream>
 
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+GLFWwindow* windowInit();
+void setCallbacks(GLFWwindow*);
+void renderLoop(GLFWwindow*);
 
 int main()
 {
+	GLFWwindow* window = windowInit();
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return NULL;
+	}
+
+	setCallbacks(window);
+
+	renderLoop(window);
+	
+	glfwTerminate();
+	return 0;
+}
+
+
+GLFWwindow* windowInit()
+{
 	glfwInit();
-	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 8);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == 0)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
+		return NULL;
 	}
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
-		glfwTerminate();
-		return -1;
+		return NULL;
 	}
 
 	glViewport(0, 0, 800, 600);
 
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	return window;
+}
 
-	// Render loop
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void setCallbacks(GLFWwindow* window)
+{
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+
+void processInput(GLFWwindow* window);
+
+void renderLoop(GLFWwindow* window)
+{
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -44,10 +74,8 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
-	glfwTerminate();
-	return 0;
 }
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {

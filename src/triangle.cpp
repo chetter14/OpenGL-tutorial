af39c5ac9d2e3ea4cfd7198cdc8ebc3940b6ad6a
Artem Leshchukov <artemleshchukov02@gmail.com>
2024-05-27 19:38:30 +0400

    face texture repeats itself, container texture clamps at its edges

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -9,10 +9,10 @@ namespace
 {
 	float vertices[] = {
 		// coords (NDC)		// colors			// texture coordinates
-		0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 1.0f,	// top right
-		0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	1.0f, 0.0f, // bottom right
+		0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	2.0f, 2.0f,	// top right
+		0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	2.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f,	// bottom left
-		-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 0.0f,	0.0f, 1.0f	// top left
+		-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 0.0f,	0.0f, 2.0f	// top left
 	};
 
 	//float rectangleVertices[] = {
@@ -77,8 +77,8 @@ std::pair<unsigned int, unsigned int> initTextures()
 	glBindTexture(GL_TEXTURE_2D, textures[0]);
 
 	// texture wrapping (what to do if texture coordinates get out of (0,0)-(1,1) span)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	// X coordinate
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);	// Y coordinate
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	// X coordinate
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);	// Y coordinate
 
 	// set options for scaling down and upwards (called texture filtering)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -101,8 +101,8 @@ std::pair<unsigned int, unsigned int> initTextures()
 	glBindTexture(GL_TEXTURE_2D, textures[1]);
 
 	// texture wrapping (what to do if texture coordinates get out of (0,0)-(1,1) span)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	// X coordinate
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);	// Y coordinate
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// X coordinate
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);	// Y coordinate
 
 	// set options for scaling down and upwards (called texture filtering)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
